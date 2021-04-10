729849ddded5c74e1fe360486ed4d7797174da4c
Lorena Arce <you@example.com>
2021-04-10 13:50:45 -0700

    able to get stock info for Google

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 12PracticeApps
 
 Going over 12 practice sample apps by using streamlit. These are all samples gone over by [Chanin Nantasenamat](https://www.youtube.com/watch?v=JwSS70SZdyM&t=5508s).
+
+[How to get stock data using python](https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75)
--- myapp.py
+++ myapp.py
@@ -5,4 +5,17 @@ import pandas as pd
 st.write("""
 # Simple Stock Price App
 
+Below you can see Google's stock price
+
 """)
+
+#To obtain the stock info we gathered the info from the article Towards Data Science that can be found in the README file
+tickerSymbol = 'GOOGL'
+# st.write(tickerSymbol)
+
+tickerData = yf.Ticker(tickerSymbol)
+# st.write(tickerData)
+
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+st.write(tickerDf)
+
