cc6bdd401e58e274d3d295fc6456d78cd919d4ac
Lorena Arce <you@example.com>
2021-04-12 15:52:09 -0700

    created dictionary and its printed text

--- Bioinformatics DNA Count/app.py
+++ Bioinformatics DNA Count/app.py
@@ -23,5 +23,39 @@ sequence = sequence.splitlines()
 # sequence
 sequence = sequence[1:]
 # sequence
+sequence  = ''.join(sequence)
+# sequence
 
+st.write("""
+***
+""")
 
+st.header('INPUT (DNA Query)')
+sequence
+
+st.header('OUTPUT (DNA Nucleotide Count)')
+
+st.subheader('1. Print dictionary')
+def DNA_nucleo_count(seq):
+    d = dict([
+        ('A', seq.count('A')),
+        ('T', seq.count('T')),
+        ('G', seq.count('G')),
+        ('C',seq.count('C'))
+        ])
+    return d
+
+X = DNA_nucleo_count(sequence)
+X
+
+# x_label = list(X)
+# x_values = list(X.values())
+# x_label
+# x_values
+
+## Have to turn count into a string because you can't concatenate a number
+st.subheader('2. Print text')
+st.write('There are ' + str(X['A']) + ' adenine (A)')
+st.write('There are  ' + str(X['T']) + ' thymine (T)')
+st.write('There are  ' + str(X['G']) + ' guanine (G)')
+st.write('There are  ' + str(X['C']) + ' cytosine (C)')
