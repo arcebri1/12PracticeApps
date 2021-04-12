c004f8d1138bd52a4eb31aac3aba5883848e703b
Lorena Arce <you@example.com>
2021-04-12 14:52:48 -0700

    setting up the neucleotide counting

--- Bioinformatics DNA Count/app.py
+++ Bioinformatics DNA Count/app.py
@@ -6,3 +6,22 @@ from PIL import Image
 image = Image.open('dna2.jpg')
 
 st.image(image, use_column_width = True)
+
+st.write("""
+# DNA Nucleotide Count Web App
+This app counts the nucleotide composition of query DNA!
+***
+""")
+
+st.header('Enter DNA Sequence')
+
+sequence_input = ">DNA Query 2\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG\nATCTTCCAGACGTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATGC\nTGAACCCGATACATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCCT"
+# sequence_input
+
+sequence = st.text_area("Sequence input", sequence_input, height=250)
+sequence = sequence.splitlines()
+# sequence
+sequence = sequence[1:]
+# sequence
+
+
