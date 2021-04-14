18b4dfdd7a234a724ca69c2a6e7e5ada3dce3b26
Lorena Arce <you@example.com>
2021-04-14 14:33:29 -0700

    added downloadable link to csv

--- EDA Basketball/app.py
+++ EDA Basketball/app.py
@@ -58,7 +58,20 @@ selected_position = st.sidebar.multiselect('Position', position_selection, posit
 filter_data_selected = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_position))]
 filter_data_selected
 
-st.header('Display Player Stats of Selected Team(s')
-st.write('Data Dimension: ' + str(filter_data_selected.shape[0]) + ' rows and ' + str(filter_data_selected.shape[1]) + 'columns')
+st.header('Display Player Stats of Selected Team(s)')
+st.write('Data Dimension: ' + str(filter_data_selected.shape[0]) + ' rows and ' + str(filter_data_selected.shape[1]) + ' columns')
 st.dataframe(filter_data_selected)
+
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+    return href
+
+
+st.markdown(filedownload(filter_data_selected), unsafe_allow_html=True)
+
+
+
+
 
