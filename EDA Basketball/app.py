c436ab92731e706c4c58f87f7fd9b317ed620a15
Lorena Arce <arcebri1@gmail.com>
2021-04-21 16:15:02 -0700

    added comments and commented out unnecessary code

--- EDA Basketball/app.py
+++ EDA Basketball/app.py
@@ -32,36 +32,41 @@ def load_data(year):
     return playerstats
 playerstats = load_data(year_to_select)
 
-url = "https://www.basketball-reference.com/leagues/NBA_" + str(year_to_select) + "_per_game.html"
-html = pd.read_html(url, header = 0)
-df = html[0]
-raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
-raw = raw.fillna(0)
-playerstats = raw.drop(['Rk'], axis=1)
-# url
-# html
-# df
-# raw
-playerstats
+# url = "https://www.basketball-reference.com/leagues/NBA_" + str(year_to_select) + "_per_game.html"
+# html = pd.read_html(url, header = 0)
+# df = html[0]
+# raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
+# raw = raw.fillna(0)
+# playerstats = raw.drop(['Rk'], axis=1)
+# # url
+# # html
+# # df
+# # raw
+# playerstats
 
 team_unique = playerstats.Tm.unique()
-team_unique
+# team_unique
 
+# Sidebar - Selected Team
 sorted_team_selection = sorted(playerstats.Tm.unique())
-sorted_team_selection
-
+# sorted_team_selection
 selected_team = st.sidebar.multiselect('Team', sorted_team_selection, sorted_team_selection)
 
+# Sidebar - Selected Position
 position_selection = ['C', 'PF', 'SF', 'PG', 'SG']
 selected_position = st.sidebar.multiselect('Position', position_selection, position_selection)
 
+# Filtering Data
 filter_data_selected = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_position))]
-filter_data_selected
+# filter_data_selected
 
+# Diplaying Data Info & Dataframe
 st.header('Display Player Stats of Selected Team(s)')
 st.write('Data Dimension: ' + str(filter_data_selected.shape[0]) + ' rows and ' + str(filter_data_selected.shape[1]) + ' columns')
 st.dataframe(filter_data_selected)
 
+# Downloading NBA data by defining a function and calling it
+# We use the article in README to get this done
 def filedownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
@@ -71,7 +76,20 @@ def filedownload(df):
 
 st.markdown(filedownload(filter_data_selected), unsafe_allow_html=True)
 
+# Creating Heatmap
+if st.button('Intercorrelation Heatmap'):
+    st.header('Intercorrelation Matrix Map')
+    filter_data_selected.to_csv('output.csv', index=False)
+    df = pd.read_csv('output.csv')
 
+    corr = df.corr()
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+    with sns.axes_style("white"):
+        f, ax = plt.subplots(figsize=(7,5))
+        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot()
 
 
 
